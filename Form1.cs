8b8d89194b3161e7fa51417f5693089539a75dfd
emtiyl <emtiyltezkry@gmail.com>
2019-06-21 17:09:44 +0300

    latest

--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,31 @@ namespace SGTranslater
 {
     public partial class Form1 : Form
     {
+        String a;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            using (CommonOpenFileDialog open = new CommonOpenFileDialog())
+            {
+                open.IsFolderPicker = true;
+                open.DefaultDirectory = "d:\\";
+                open.InitialDirectory = "d:\\";
+                if (open.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    a = open.FileName;
+                }
+                MessageBox.Show(a);
+            }
         }
     }
 }
