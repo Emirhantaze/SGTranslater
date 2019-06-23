823b1cf398a21eb7d52d41691f12776e69dcb922
emtiyl <emtiyltezkry@gmail.com>
2019-06-23 21:06:07 +0300

    06\23 update

--- Form1.cs
+++ Form1.cs
@@ -14,52 +14,27 @@ namespace SGTranslater
 {
     public partial class Form1 : Form
     {
-        String a;
+        
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-          
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            using (CommonOpenFileDialog open = new CommonOpenFileDialog())
-            {
-                open.IsFolderPicker = false;
-                open.Multiselect = true;
-
-                open.DefaultDirectory = "d:\\";
-                open.InitialDirectory = "d:\\";
-                if (open.ShowDialog() == CommonFileDialogResult.Ok)
-                {
-                    a = open.FileName;
-                }
-                MessageBox.Show(a);
-            }
-        }
-
-        private void Button2_Click(object sender, EventArgs e)
-        {
-            Dosyalar dosya = new Dosyalar(".\\save.txt");
-            label1.Text = dosya.text;
+            Application.Exit();
         }
 
         private void Button3_Click(object sender, EventArgs e)
         {
-            Form2 form2 = new Form2();
-            form2.Show();
-            this.Visible = false;
 
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        private void Form1_Load_1(object sender, EventArgs e)
         {
-            Application.Exit();
+            Dosyalar dosya = new Dosyalar(".\\save.txt");
+            button3.Text = "Yeni Dosya " + Environment.NewLine + "Üretmek için" + Environment.NewLine + "Tıklayın.";
         }
     }
 }
--- dosyalar.cs
+++ dosyalar.cs
@@ -10,22 +10,41 @@ namespace SGTranslater
 {
     class Dosyalar
     {
-        public string text;
+        public string text,loadfile;
         public string[] lines;
         public List<string[]> liness = new List<string[]>();
-         public Dosyalar(string loadfile)
+        public Dosyalar(string loadfile)
         {
             text = File.ReadAllText(loadfile);
             this.lines = File.ReadAllLines(loadfile);
-            
-          
+
+            loadfile = this.loadfile;
             for (int i = 0; lines.Length>i;i++)
             {
                liness.Add( lines[i].Split(','));
-                Console.WriteLine(liness[i].Length);
+                
             }
         }
-       
+       public void write()
+        {
+            text = "";
+            for(int i = 0; liness.Count > i; i++)
+            {
+                for (int j = 0; liness.ElementAt(i).Length > j; j++)
+                {
+                    if (liness.ElementAt(i).Length - 1 == j)
+                    {
+                        text = text + liness.ElementAt(i)[j];
+                    }
+                    else
+                    {
+                        text = text + liness.ElementAt(i)[j]+",";
+                    }
+                }
+                text = text+ Environment.NewLine;
+            }
+            File.WriteAllText(loadfile,text);
+        }
         
     }
 }
